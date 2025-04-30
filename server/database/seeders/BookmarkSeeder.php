0b8376c8c6dc59371020fbdcffa6ad5c3541e625
hassan <hassanak509@gmail.com>
2025-04-30 22:37:45 +0300

    created bookmark seeder to generate sample bookmark battles

---
+++ server/database/seeders/BookmarkSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bookmark;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookmarkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Bookmark::factory()->count(5)->create();
+    }
+}
