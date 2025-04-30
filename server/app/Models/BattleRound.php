b780c43a03f115fe09ba8022d34af21779b1dbb1
hassan <hassanak509@gmail.com>
2025-04-30 11:28:14 +0300

    created battle_round model with relationships

---
+++ server/app/Models/BattleRound.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BattleRound extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['battle_id', 'round_number'];
+
+    public function battle()
+    {
+        return $this->belongsTo(Battle::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(BattleResponse::class);
+    }
+}
