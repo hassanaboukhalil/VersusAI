846fb1140f6f906b5c725205134363002bc471fe
hassan <hassanak509@gmail.com>
2025-04-30 11:27:48 +0300

    created battle_response model with relationships

---
+++ server/app/Models/BattleResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BattleResponse extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['battle_round_id', 'ai_model_id', 'response_text'];
+
+    public function round()
+    {
+        return $this->belongsTo(BattleRound::class, 'battle_round_id');
+    }
+
+    public function aiModel()
+    {
+        return $this->belongsTo(AiModel::class);
+    }
+}
