d50c1b8e13b16fb9df9c330161e734bd6ceb35f5
hassan <hassanak509@gmail.com>
2025-05-02 21:40:23 +0300

    Initialized Next frontend using npm

---
+++ client/postcss.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+    plugins: {
+        '@tailwindcss/postcss': {},
+        autoprefixer: {}, // Autoprefixer is a PostCSS plugin that automatically adds the correct vendor prefixes to your CSS, so it works consistently across different browsers. for example on firefox, old browsers ...
+    },
+};
+
+export default config;
