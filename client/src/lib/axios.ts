7c40681b0e8bed3f07f1c3fb5c7e20c0667c0224
hassan <hassanak509@gmail.com>
2025-05-03 23:43:29 +0300

    updated the url in the axios.ts file

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -4,7 +4,7 @@ const token = localStorage.getItem('token');
 
 const api = axios.create({
     // baseURL: 'http://13.37.211.34/api/v1',
-    baseURL: 'http://localhost/api/v1',
+    baseURL: 'http://127.0.0.1:8000/api/v1',
 
     headers: {
         'Content-Type': 'application/json',
