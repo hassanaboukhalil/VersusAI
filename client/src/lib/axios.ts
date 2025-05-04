5725c0c374f7f230a16dd6a75b36299144ebe4ac
hassan <hassanak509@gmail.com>
2025-05-04 23:02:38 +0300

    refactored api setup to use request interceptor for dynamic token injection

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,15 +1,22 @@
 import axios from 'axios';
-
-const token = localStorage.getItem('token');
+import { getUser } from './auth';
 
 const api = axios.create({
-    // baseURL: 'http://13.37.211.34/api/v1',
+    // baseURL: 'http://127.0.0.1:8000/api/v1',
     baseURL: 'http://127.0.0.1:8000/api/v1',
-
     headers: {
         'Content-Type': 'application/json',
-        ...(token && { Authorization: `Bearer ${token}` }),
     },
 });
+
+if (typeof window !== 'undefined') {
+    api.interceptors.request.use((config) => {
+        const user = getUser();
+        if (user?.token) {
+            config.headers.Authorization = `Bearer ${user.token}`;
+        }
+        return config;
+    });
+}
 
 export default api;
