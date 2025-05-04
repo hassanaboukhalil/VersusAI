29e8739729b0a8e455c431b7f7a0864b91e43ae4
hassan <hassanak509@gmail.com>
2025-05-04 10:08:27 +0300

    added a default value of user regarding user type

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_type_id');
+            $table->foreignId('user_type_id')->default(2);
             $table->string('first_name');
             $table->string('last_name');
             $table->string('username')->unique();
