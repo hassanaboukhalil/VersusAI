ab08717f9cb4ffd4f23c59defc7705f2a9a347e7
hassan <hassanak509@gmail.com>
2025-05-07 21:24:24 +0300

    backend: created BattleService class for future battle-related logic

---
+++ server/app/Services/BattleService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\app\Services;
+
+class BattleService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
