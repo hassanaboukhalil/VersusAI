c1f23a1bbc2b4e73724a3f6ea1e5adfff6efe45a
hassan <hassanak509@gmail.com>
2025-05-07 11:42:03 +0300

    updated frontend axios config to use BASE_URL from environment variables

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -3,7 +3,8 @@ import { getUser } from './auth';
 
 const api = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api/v1',
-    baseURL: 'http://127.0.0.1:8000/api/v1',
+    // baseURL: 'http://16.232.122.1/:8000/api/v1',
+    baseURL: process.env.BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
