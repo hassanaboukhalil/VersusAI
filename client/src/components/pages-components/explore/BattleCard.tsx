6387c74be7bb8cfcfc568c063beea959056c9b8d
hassan <hassanak509@gmail.com>
2025-05-08 00:03:39 +0300

    frontend: extracted Battle type into shared types file

--- client/src/components/pages-components/explore/BattleCard.tsx
+++ client/src/components/pages-components/explore/BattleCard.tsx
@@ -1,15 +1,6 @@
 import Image from 'next/image';
 import { Button } from '../../ui/button';
-
-interface Battle {
-    id: string;
-    title: string;
-    models: string[];
-    votes: Record<string, number>;
-    type: string;
-    date: string;
-    user: { name: string; avatar: string };
-}
+import { Battle } from '../../../types/battle';
 
 const BattleCard = ({ battle }: { battle: Battle }) => {
     return (
