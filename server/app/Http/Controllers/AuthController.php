31cea76972726c2e0a540fac44940a1e36cf3ed4
hassan <hassanak509@gmail.com>
2025-05-07 14:06:48 +0300

    backend: added login method to AuthController using AuthService

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Services\AuthService;
 
@@ -16,6 +17,21 @@ class AuthController extends Controller
                 return $this->successResponse($user);
             }
             return $this->errorResponse("Invalid request", 400);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500);
+        }
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            $authService = new AuthService();
+            $data = $authService->login($request);
+
+            if ($data) {
+                return $this->successResponse($data, 'Login successful');
+            }
+            return $this->errorResponse('Invalid credentials', 401);
         } catch (\Exception $e) {
             return $this->errorResponse($e->getMessage(), 500);
         }
