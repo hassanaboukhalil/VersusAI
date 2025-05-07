fc007a71684076d921462adb8de329d17bb1193a
hassan <hassanak509@gmail.com>
2025-05-08 01:51:02 +0300

    frontend: removed unused redux hooks from store definition

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,6 +1,4 @@
-// src/redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 import exploreReducer from './slices/exploreSlice';
 
 export const store = configureStore({
@@ -9,10 +7,4 @@ export const store = configureStore({
     },
 });
 
-// Types for dispatch and selector hooks
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-// Hooks to use across the app
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
