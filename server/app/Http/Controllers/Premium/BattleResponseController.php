cddd864cb38b974db56932023a2b381eadb366ce
hassan <hassanak509@gmail.com>
2025-05-08 18:27:34 +0300

    backend: added getBattleResponse method to BattleResponseController

--- server/app/Http/Controllers/Premium/BattleResponseController.php
+++ server/app/Http/Controllers/Premium/BattleResponseController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Premium;
 
 use App\Http\Controllers\Controller;
+use App\Services\BattleService;
 use Illuminate\Http\Request;
 
 class BattleResponseController extends Controller
 {
-    //
+    public function getBattleResponse()
+    {
+        $battle_service = new BattleService();
+
+        $battle_response = $battle_service->getBattleResponse();
+    }
 }
