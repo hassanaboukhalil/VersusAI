e63e5e7262bef5d3590a0896399b75d8ac294112
hassan <hassanak509@gmail.com>
2025-05-08 21:32:29 +0300

    backend: added a function called getTextSummarizationResponse in the BattleResponseService file

--- server/app/Services/BattleResponseService.php
+++ server/app/Services/BattleResponseService.php
@@ -6,4 +6,48 @@ use App\Schemas\BattleResponseSchema;
 use Prism\Prism\Enums\Provider;
 use Prism\Prism\Prism;
 
-class BattleResponseService {}
+class BattleResponseService
+{
+    public function getTextSummarizationResponse($ai_model_name, $text_to_summarize) // $ai_model_name, $battle_type
+    {
+        $schema = BattleResponseSchema::createPrismSchema(
+            "text_summarization",
+            "Summarize a text",
+            [
+                "name" => "Text Summarization",
+                "description" => "Summarize the text",
+            ]
+        );
+
+        if ($ai_model_name === 'gpt-4o' || $ai_model_name === 'gpt-4o' || $ai_model_name === 'gpt-4.1' || $ai_model_name === 'chatgpt-4o' || $ai_model_name === 'o3-mini') {
+            $response = Prism::structured()
+                ->using(Provider::OpenAI, $ai_model_name) //'gpt-4o'
+                ->withSchema($schema)
+                ->withPrompt($text_to_summarize)
+                ->asStructured();
+        }
+
+        return $response->structured;
+    }
+
+
+    // public function getTextSummarizationResponse() // $ai_model_name, $battle_type
+    // {
+    //     $schema = BattleResponseSchema::createPrismSchema(
+    //         "joke",
+    //         "A joke about programming",
+    //         [
+    //             "setup" => "This is the setup of the joke",
+    //             "punchline" => "This is the punchline",
+    //         ]
+    //     );
+
+    //     $response = Prism::structured()
+    //         ->using(Provider::OpenAI, 'gpt-4o')
+    //         ->withSchema($schema)
+    //         ->withPrompt('Give me a funny programming joke')
+    //         ->asStructured();
+
+    //     return $response->structured;
+    // }
+}
