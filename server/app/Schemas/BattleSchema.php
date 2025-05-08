41f7b46685c9b843088281cd0f93b8794eb4d354
hassan <hassanak509@gmail.com>
2025-05-08 17:47:30 +0300

    backend: added createPrismSchema method to BattleSchema for dynamic ObjectSchema generation

--- server/app/Schemas/BattleSchema.php
+++ server/app/Schemas/BattleSchema.php
@@ -9,7 +9,19 @@ use Prism\Prism\Schema\StringSchema;
 class BattleSchema
 {
 
+    public static function createPrismSchema($name, $description, $properties)
+    {
 
+
+        $schema = new ObjectSchema(
+            name: $name,
+            description: $description,
+            properties: self::createProperties($properties),
+            requiredFields: array_keys($properties)
+        );
+
+        return $schema;
+    }
 
 
     public static function createProperties($properties)
