decc2e55dcc0819118791b0bc95cc701359e3810
hassan <hassanak509@gmail.com>
2025-05-08 20:53:07 +0300

    backend: removed getBattleResponse method from BattleService

--- server/app/Services/BattleService.php
+++ server/app/Services/BattleService.php
@@ -3,9 +3,6 @@
 namespace App\Services;
 
 use App\Models\Battle;
-use App\Schemas\BattleResponseSchema;
-use Prism\Prism\Enums\Provider;
-use Prism\Prism\Prism;
 
 class BattleService
 {
@@ -45,26 +42,5 @@ class BattleService
         }
 
         return $battles;
-    }
-
-
-    public function getBattleResponse() // $ai_model_name, $battle_type
-    {
-        $schema = BattleResponseSchema::createPrismSchema(
-            "Text Summarization",
-            "Summarize a text",
-            [
-                "name" => "This name of the task",
-                "description" => "Summarize the text",
-            ]
-        );
-
-        $response = Prism::structured()
-            ->using(Provider::OpenAI, 'gpt-4o')
-            ->withSchema($schema)
-            ->withPrompt('Creating an LLM-powered mobile app begins by carefully selecting a strong and reliable open-source model like LLaMA, Mistral, or Gemma to fit the projects needs. Instead of building a model from scratch, using an existing base model saves significant time and resources while ensuring high-quality performance. To enable communication between the app and the model, a backend using FastAPI or Laravel is built, with the LLM being served through a system like Hugging Face Inference or Ollama. A custom AI agent is then developed on top of the model to manage conversation flow, add personality, and ensure a smooth, human-like interaction. Through the mobile app, users can chat directly with the AI in real time, creating a seamless experience similar to what ChatGPT provides, but fully customized to the projects vision.')
-            ->asStructured();
-
-        return $response->structured;
     }
 }
