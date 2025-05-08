7812139e575116a2d0cc1da48380dec658bd1919
hassan <hassanak509@gmail.com>
2025-05-08 17:46:32 +0300

    backend: created a dynamic createProperties method in the BattleSchema.php file

--- server/app/Schemas/BattleSchema.php
+++ server/app/Schemas/BattleSchema.php
@@ -4,3 +4,22 @@ namespace App\Schemas;
 
 use Prism\Prism\Schema\ObjectSchema;
 use Prism\Prism\Schema\StringSchema;
+
+
+class BattleSchema
+{
+
+
+
+
+    public static function createProperties($properties)
+    {
+
+        $props = [];
+        foreach ($properties as $key => $value) {
+            $props[] = new StringSchema($key, $value);
+        }
+
+        return $props;
+    }
+}
