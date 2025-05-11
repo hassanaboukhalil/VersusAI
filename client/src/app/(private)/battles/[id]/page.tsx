7e7c92e06980251428de12b34ceff5dc27cd72df
hassan <hassanak509@gmail.com>
2025-05-11 12:01:28 +0300

    frontend: refactored BattleDetailsPage to support multiple rounds and improved layout with Section component

--- client/src/app/(private)/battles/[id]/page.tsx
+++ client/src/app/(private)/battles/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'next/navigation';
 import { Button } from '../../../../components/ui/button';
 import { Input } from '../../../../components/ui/input';
 import Image from 'next/image';
+import Section from '../../../../components/layout/Section';
 
 const BattleDetailsPage = () => {
     const { id } = useParams();
@@ -15,23 +16,39 @@ const BattleDetailsPage = () => {
         title: 'Summarize a text about creating LLM',
         description:
             'Creating a Large Language Model (LLM)-powered application starts by selecting a suitable open-source base model that fits the project’s goals...',
-        results: [
-            {
-                ai: 'Chatgpt',
-                summary:
-                    'To create an LLM-powered app, start by selecting a strong open-source model like LLaMA or Mistral...',
-                subSummary:
-                    'Building an AI chatbot application starts with choosing a suitable open-source LLM...',
-                votes: 122,
-            },
-            {
-                ai: 'Gemini',
-                summary:
-                    'Creating LLM-powered models begins by carefully selecting a strong and reliable open-source model...',
-                subSummary:
-                    'Developing an intelligent chat application powered by an LLM involves several important steps...',
-                votes: 100,
-            },
+        rounds: [
+            [
+                {
+                    ai: 'Chatgpt',
+                    responseText:
+                        'To create an LLM-powered app, start by selecting a strong open-source model like LLaMA or Mistral, To create an LLM-powered app, start by selecting a strong open-source model like LLaMA or Mistral, To create an LLM-powered app, start by selecting a strong open-source model like LLaMA or Mistral',
+                    votes: 122,
+                    src: '/images/llms/openai.svg',
+                },
+                {
+                    ai: 'Gemini',
+                    responseText:
+                        'Creating LLM-powered models begins by carefully selecting a strong and reliable open-source model...',
+                    votes: 100,
+                    src: '/images/llms/gemini.svg',
+                },
+            ],
+            [
+                {
+                    ai: 'Chatgpt',
+                    responseText:
+                        'To create an LLM-powered app, start by selecting a strong open-source model like LLaMA or Mistral...',
+                    votes: 122,
+                    src: '/images/llms/openai.svg',
+                },
+                {
+                    ai: 'Gemini',
+                    responseText:
+                        'Creating LLM-powered models begins by carefully selecting a strong and reliable open-source model...',
+                    votes: 100,
+                    src: '/images/llms/gemini.svg',
+                },
+            ],
         ],
         comments: [
             {
@@ -48,34 +65,39 @@ const BattleDetailsPage = () => {
     };
 
     return (
-        <div className="bg-background text-white px-6 py-10 max-w-5xl mx-auto">
-            <h1 className="text-2xl font-bold mb-2">{battle.title}</h1>
-            <p className="text-gray-300 mb-6">{battle.description}</p>
+        <Section className="bg-background text-white px-6 py-10 mx-auto">
+            <h1 className="text-2xl font-bold">{battle.title}</h1>
+            <p className="text-gray-300 mt-8 mb-6">{battle.description}</p>
 
-            <div className="border border-lime-300 p-4 space-y-8 rounded-md">
-                {battle.results.map((res, i) => (
-                    <div key={i}>
-                        <h3 className="text-lg font-semibold mb-2">Result {i + 1}</h3>
-                        <div className="bg-white text-black p-3 rounded mb-2 text-sm">
-                            {res.summary}
-                        </div>
-                        <div className="bg-white text-black p-3 rounded text-sm">
-                            {res.subSummary}
-                        </div>
-                        {ended && (
-                            <div className="mt-2 flex justify-end">
-                                <Button size="icon" className="bg-primary text-black">
-                                    +
-                                </Button>
+            <div className="border border-lime-300 p-4 space-y-8 rounded-md bg-dark-white flex flex-col gap-2">
+                {battle.rounds.map((round, i) => (
+                    <div key={i} className="my-8">
+                        <h3 className="text-lg font-semibold">Result {i + 1}</h3>
+
+                        <div className="flex flex-col gap-4 mt-6">
+                            <div className="flex w-full items-start gap-2">
+                                <Image src={round[0].src} alt="ai model 1" width="50" height="50" />
+                                <div className="bg-white text-black p-3 rounded mb-2 text-sm max-w-[85%]">
+                                    {round[0].responseText}
+                                </div>
                             </div>
-                        )}
+                            <div className="flex w-full items-start justify-start flex-row-reverse gap-2">
+                                <Image src={round[1].src} alt="ai model 1" width="50" height="50" />
+                                <div className="bg-white text-black p-3 rounded mb-2 text-sm w-[85%]">
+                                    {round[1].responseText}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
 
             {!ended ? (
-                <div className="flex justify-center mt-6">
-                    <Button className="bg-primary text-black" onClick={() => setEnded(true)}>
+                <div className="flex justify-start mt-6 gap-4">
+                    <Button className="bg-[#2C2C2C] text-white border border-[#DEFE01] hover:bg-[#dcfe0198]">
+                        Give another results
+                    </Button>
+                    <Button variant="default" onClick={() => setEnded(true)}>
                         End Battle
                     </Button>
                 </div>
@@ -84,7 +106,7 @@ const BattleDetailsPage = () => {
                     {/* Votes Section */}
                     <div className="mt-6">
                         <h3 className="text-xl font-semibold mb-2">Votes</h3>
-                        <div className="flex items-center gap-4">
+                        <div className="flex items-center gap-4 text-right">
                             <Button className="bg-primary text-black">
                                 Vote for {battle.results[0].ai}
                             </Button>
@@ -127,7 +149,7 @@ const BattleDetailsPage = () => {
                     </div>
                 </>
             )}
-        </div>
+        </Section>
     );
 };
 
