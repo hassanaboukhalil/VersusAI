7dbf020af4178e1e7ca082db1028bbf789a95669
hassan <hassanak509@gmail.com>
2025-05-11 19:00:35 +0300

    backend: added get battle endpoint to BattleController and documented create request structure

--- server/app/Http/Controllers/BattleController.php
+++ server/app/Http/Controllers/BattleController.php
@@ -23,6 +23,17 @@ class BattleController extends Controller
     {
         $battle_service = new BattleService();
 
+        /*
+        {
+            $title = 'Text Summarization Battle Between Two AIs',
+            $description = 'Creating an LLM-powered mobile app begins by carefully selecting a strong and reliable open-source model like LLaMA, Mistral, or Gemma to fit the projects needs. Instead of building a model from scratch, using an existing base model saves significant time and resources while ensuring high-quality performance. To enable communication between the app and the model, a backend using FastAPI or Laravel is built, with the LLM being served through a system like Hugging Face Inference or Ollama. A custom AI agent is then developed on top of the model to manage conversation flow, add personality, and ensure a smooth, human-like interaction. Through the mobile app, users can chat directly with the AI in real time, creating a seamless experience similar to what ChatGPT provides, but fully customized to the projects vision.
+            $battle_type_name = 'Text Summarization',
+            $ai_model_1_name = 'gemini-2.0-flash',
+            $ai_model_2_name = 'deepseek-prover-v2',
+
+        }
+
+        */
         $battle = $battle_service->createBattle($request);
 
         if ($battle) {
@@ -30,6 +41,18 @@ class BattleController extends Controller
                 $battle,
                 'Battle created successfully.'
             );
+        }
+
+        return $this->errorResponse('Something went wrong, try again', 500);
+    }
+
+    public function get(Request $request)
+    {
+        $battle_service = new BattleService();
+        $battle = $battle_service->getBattle($request);
+
+        if ($battle) {
+            return $this->successResponse($battle);
         }
 
         return $this->errorResponse('Something went wrong, try again', 500);
