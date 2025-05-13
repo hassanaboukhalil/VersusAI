2305438cbba505b91a6012e115971c048106986c
hassan <hassanak509@gmail.com>
2025-05-13 19:41:06 +0300

    backend: added votes array and battleId property to VoteUpdated event class for enhanced vote tracking

--- server/app/Events/VoteUpdated.php
+++ server/app/Events/VoteUpdated.php
@@ -8,4 +8,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 class VoteUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public array $votes;
+    private int $battleId;
 }
