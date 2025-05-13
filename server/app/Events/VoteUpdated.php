fceb8c2ee1047a495d5702df39271672f7038896
hassan <hassanak509@gmail.com>
2025-05-13 19:44:48 +0300

    backend: enhanced VoteUpdated event class with broadcastWith and broadcastAs methods for improved broadcasting data and event naming

--- server/app/Events/VoteUpdated.php
+++ server/app/Events/VoteUpdated.php
@@ -2,7 +2,14 @@
 
 namespace App\Events;
 
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 
 
 class VoteUpdated implements ShouldBroadcast
@@ -18,10 +25,25 @@ class VoteUpdated implements ShouldBroadcast
         $this->votes = $votes;
     }
 
+    // Get the channels the event should broadcast on
     public function broadcastOn(): array
     {
         return [
             new PrivateChannel('battle.' . $this->battleId),
         ];
+    }
+
+    // The data to broadcast.
+    public function broadcastWith(): array
+    {
+        return [
+            'votes' => $this->votes,
+        ];
+    }
+
+    // The event's broadcast name.
+    public function broadcastAs(): string
+    {
+        return 'vote.updated';
     }
 }
