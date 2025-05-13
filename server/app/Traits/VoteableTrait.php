4f156c7c43d1642a63d77d05699c5050c1c62561
hassan <hassanak509@gmail.com>
2025-05-13 18:02:47 +0300

    backend: added getVotesCount method to VoteableTrait for retrieving total votes, enhancing voting functionality across models

--- server/app/Traits/VoteableTrait.php
+++ server/app/Traits/VoteableTrait.php
@@ -4,4 +4,11 @@ namespace App\Traits;
 
 use App\Models\Vote;
 
-trait VoteableTrait {}
+trait VoteableTrait
+{
+    // Get the total votes for this model
+    public function getVotesCount(): int
+    {
+        return $this->votes()->count();
+    }
+}
