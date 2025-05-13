1ce16aec9ab3e7a29f452d2e69e58af03a52318c
hassan <hassanak509@gmail.com>
2025-05-13 19:40:39 +0300

    backend: enhanced VoteUpdated event class with Dispatchable, InteractsWithSockets, and SerializesModels traits for improved real-time broadcasting functionality

--- server/app/Events/VoteUpdated.php
+++ server/app/Events/VoteUpdated.php
@@ -5,4 +5,7 @@ namespace App\Events;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 
-class VoteUpdated implements ShouldBroadcast {}
+class VoteUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+}
