496ec12b04fe4839873197c3f027a009bbb15a07
hassan <hassanak509@gmail.com>
2025-05-14 22:53:53 +0300

    backend: implemented socket connection handling with vote event broadcasting and user connection logging

--- realtime-server/index.js
+++ realtime-server/index.js
@@ -13,3 +13,15 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("vote", (data) => {
+    io.emit(`vote_update_${data.battleId}`, data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
