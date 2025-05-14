5b12edcf8229a6d7819b4e16f7e1a286bf73360d
hassan <hassanak509@gmail.com>
2025-05-14 15:20:20 +0300

    backend: implemented pay method in PaymentController for processing payments via Stripe

--- server/app/Http/Controllers/PaymentController.php
+++ server/app/Http/Controllers/PaymentController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+    public function payment()
+    {
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+        $stripe->charges->create([
+            'amount' => 1000,
+            'currency' => 'usd',
+            'source' => 'tok_mastercard',
+            'description' => 'My First Test Charge',
+        ]);
+    }
 }
