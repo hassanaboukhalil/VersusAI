847497b6b6b7c2c28f2d58cc397b636601fb19a0
hassan <hassanak509@gmail.com>
2025-05-14 10:05:13 +0300

    frontend: updated pricing page header to use a more semantic h2 element for improved accessibility and styling

--- client/src/app/(private)/pricing/page.tsx
+++ client/src/app/(private)/pricing/page.tsx
@@ -5,7 +5,7 @@ import { PricingCard } from '../../../components/global/PricingCard';
 const page = () => {
     return (
         <Section className="bg-background min-h-screen py-12">
-            <h1 className="text-white text-3xl font-bold mb-8">Upgrade your account</h1>
+            <h2 className="h2 font-bold text-center">Upgrade your account</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto w-fit mt-16">
                 {PRICING_PLANS.map((plan) => (
                     <PricingCard key={plan.name} plan={plan} />
