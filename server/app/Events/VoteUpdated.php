c2f7a2e94cad720352b467ffbe1e0378e5baebb7
hassan <hassanak509@gmail.com>
2025-05-14 06:41:45 +0300

    frontend: updated Echo subscription in BattleDetailsPage to use public channels

--- server/app/Events/VoteUpdated.php
+++ server/app/Events/VoteUpdated.php
@@ -29,7 +29,7 @@ class VoteUpdated implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('battle.' . $this->battleId),
+            new Channel('battle.' . $this->battleId),
         ];
     }
 
