4d44461f72677c470218216d103c55381e16d03c
hassan <hassanak509@gmail.com>
2025-05-14 17:53:22 +0300

    frontend: updated axios base URL to use environment variable for improved configuration and flexibility

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { getUser } from './auth';
 
-// const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`;
-const BASE_URL = `http://35.181.172.250:8000/api/v1`; // http://127.0.0.1:8000 http://15.237.122.116
+const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`;
+// const BASE_URL = `http://35.181.172.250:8000/api/v1`; // http://127.0.0.1:8000 http://15.237.122.116
 
 const api = axios.create({
     baseURL: BASE_URL,
