a07850f263454fe94cd73df382ed930c3f16d2f1
hassan <hassanak509@gmail.com>
2025-05-14 17:48:37 +0300

    frontend: updated payment success page to include routing after payment verification and improved UI structure

--- client/src/app/(public)/payment-success/page.tsx
+++ client/src/app/(public)/payment-success/page.tsx
@@ -1,11 +1,16 @@
-import { useSearchParams } from 'next/navigation';
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import api from '../../../lib/axios';
+import Logo from '../../../components/layout/Logo';
+import Section from '../../../components/layout/Section';
 
 const PaymentSuccessPage = () => {
     const searchParams = useSearchParams();
     const sessionId = searchParams.get('session_id');
     const [status, setStatus] = useState('Verifying payment...');
+    const router = useRouter();
 
     useEffect(() => {
         const verifyPayment = async () => {
@@ -17,12 +22,13 @@ const PaymentSuccessPage = () => {
                 //     { withCredentials: true }
                 // );
 
-                const res = await api.get('/payment-success?session_id=${sessionId}', {
+                const res = await api.get(`/payment-success?session_id=${sessionId}`, {
                     withCredentials: true,
                 });
 
                 if (res.data.message) {
                     setStatus('✅ Payment successful and recorded.');
+                    router.push('/explore');
                 } else {
                     setStatus('⚠️ Could not verify payment.');
                 }
@@ -36,10 +42,11 @@ const PaymentSuccessPage = () => {
     }, [sessionId]);
 
     return (
-        <div className="p-10 text-center">
-            <h1 className="text-3xl font-bold text-green-600">Payment Result</h1>
+        <Section className="p-10 text-center h-screen flex flex-col items-center justify-center gap-4">
+            <Logo withTitle={true} />
+            <h1 className="text-3xl font-bold text-green-600 mt-10">Payment Result</h1>
             <p className="mt-4">{status}</p>
-        </div>
+        </Section>
     );
 };
 export default PaymentSuccessPage;
