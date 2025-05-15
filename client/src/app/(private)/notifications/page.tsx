5a697ae449fcc8bff1c69f4ef83c20bd8fee3e8c
hassan <hassanak509@gmail.com>
2025-05-15 15:29:30 +0300

    frontend: added sample notifications data to Notifications page component for testing and display purposes

--- client/src/app/(private)/notifications/page.tsx
+++ client/src/app/(private)/notifications/page.tsx
@@ -1,6 +1,27 @@
 import Section from '../../../components/layout/Section';
 
 const page = () => {
+    const notifications = [
+        {
+            id: 1,
+            type: 'follow',
+            user: {
+                name: 'Yehya Lorem',
+                avatar: '/images/no-user-profile-pic.jpeg',
+            },
+            message: 'started following you.',
+        },
+        ...Array(10).fill({
+            id: Math.random(),
+            type: 'vote',
+            user: {
+                name: 'Yehya Lorem',
+                avatar: '/images/no-user-profile-pic.jpeg',
+            },
+            message: 'voted on the battle titled by "Onsite or Remote Work"',
+        }),
+    ];
+
     return (
         <Section className="bg-background min-h-screen py-12">
             <h1 className="text-white text-3xl font-bold mb-8">Notifications</h1>
