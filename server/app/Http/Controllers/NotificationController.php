c25204628151086e538126d67bbe5d2243b8e90a
hassan <hassanak509@gmail.com>
2025-05-15 16:03:38 +0300

    backend: implemented getNotifications method in NotificationController to fetch user notifications

--- server/app/Http/Controllers/NotificationController.php
+++ server/app/Http/Controllers/NotificationController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\NotificationService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
-    //
+    public function getNotifications()
+    {
+        $user = Auth::user();
+
+        $notificationsService = new NotificationService();
+        $notifications = $notificationsService->getAllForUser($user->id);
+
+        return $this->successResponse($notifications, 'Notifications fetched successfully.');
+    }
 }
