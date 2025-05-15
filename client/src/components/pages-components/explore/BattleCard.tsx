1ad1cff45db73e89fc5f725142ebe2879959b32e
hassan <hassanak509@gmail.com>
2025-05-15 11:55:07 +0300

    frontend: simplified BattleCard component by removing commented-out code to enhance readability and maintainability

--- client/src/components/pages-components/explore/BattleCard.tsx
+++ client/src/components/pages-components/explore/BattleCard.tsx
@@ -6,41 +6,6 @@ import Link from 'next/link';
 const BattleCard = ({ battle }: { battle: Battle }) => {
     return (
         <div className="bg-dark-white rounded-lg p-4 text-white border border-white/10 flex flex-col justify-between h-full gap-6">
-            {/* <div>
-                <h3 className="text-primary text-2xl font-semibold">{battle.title}</h3>
-                <p className="text-base font-medium mt-6">
-                    {battle.ai_model_1_name} vs {battle.ai_model_2_name}
-                </p>
-
-                <div className="text-sm text-gray-300 flex justify-between mt-4">
-                    <div>
-                        <div className="text-lg">Votes</div>
-                        <div className="text-sm mt-2">
-                            {battle.ai_model_1_name}: {battle.votes_ai_model_1}
-                            <br />
-                            {battle.ai_model_2_name}: {battle.votes_ai_model_2}
-                        </div>
-                    </div>
-                    <div>
-                        <span className="text-primary text-lg">{battle.type}</span>
-                    </div>
-                </div>
-
-                <div className="flex justify-between items-center mt-4">
-                    <div className="flex items-center gap-2">
-                        <Image
-                            src={'/images/no-user-profile-pic.jpeg'}
-                            alt={`${battle.user_first_name} ${battle.user_last_name}`}
-                            width={35}
-                            height={35}
-                            className="rounded-full"
-                        />
-                        <span className="text-lg">
-                            {battle.user_first_name} {battle.user_last_name}
-                        </span>
-                    </div>
-                </div>
-            </div> */}
             <h3 className="text-primary text-2xl font-semibold">{battle.title}</h3>
 
             <div className="w-full">
