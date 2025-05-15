803fb0258f18bcee74b5a9f0162e45d8efbe176c
hassan <hassanak509@gmail.com>
2025-05-15 15:51:17 +0300

    frontend: added notification reducer to Redux store for managing user notifications

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import exploreReducer from './slices/exploreSlice';
 import battleReducer from './slices/battleSlice';
+import notificationReducer from './slices/notificationSlice';
 
 export const store = configureStore({
     reducer: {
         explore: exploreReducer,
         battle: battleReducer,
+        notification: notificationReducer,
     },
 });
 
