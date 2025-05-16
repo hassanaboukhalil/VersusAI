4ddf98e1cfbbf0db71bab81fcb1ffc6a48960276
hassan <hassanak509@gmail.com>
2025-05-17 01:59:04 +0300

    frontend: adjusted margin in Pricing component for improved layout consistency

--- client/src/components/pages-components/index/Pricing.tsx
+++ client/src/components/pages-components/index/Pricing.tsx
@@ -4,7 +4,7 @@ import { PricingCard } from '../../global/PricingCard';
 
 const Pricing = () => {
     return (
-        <Section className="py-16 text-white mt-52" id="pricing">
+        <Section className="py-16 text-white mt-24" id="pricing">
             <h2 className="h2 font-bold text-center">Pricing Tiers</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto w-fit mt-16">
                 {PRICING_PLANS.map((plan) => (
