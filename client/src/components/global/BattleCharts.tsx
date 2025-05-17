5e151f2a379f168c906fc9f4dc5c0f06dc47247c
hassan <hassanak509@gmail.com>
2025-05-17 03:00:35 +0300

    frontend: added Round type and makeChartData function to BattleCharts component for improved data processing and visualization

--- client/src/components/global/BattleCharts.tsx
+++ client/src/components/global/BattleCharts.tsx
@@ -11,3 +11,22 @@ type Response = {
     response_time_ms: number;
     completion_tokens: number;
 };
+
+export type Round = { id: number; responses: Response[] };
+type Props = { rounds: Round[] };
+
+const COLORS = ['#22d3ee', '#c084fc'];
+
+const makeChartData = (rounds: Round[]) => {
+    rounds.map((r, i) =>
+        r.responses.reduce(
+            (acc, cur) => ({
+                ...acc,
+                name: `Round ${i + 1}`,
+                [`${cur.ai_model_name}_response`]: cur.response_time_ms,
+                [`${cur.ai_model_name}_tokens`]: cur.completion_tokens,
+            }),
+            {} as Record<string, string | number>
+        )
+    );
+};
