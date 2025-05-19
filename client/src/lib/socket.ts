1c68a3128f748c70a053d11a8d3dd68e7ea318ed
hassan <hassanak509@gmail.com>
2025-05-19 03:48:37 +0300

    frontend: updated socket connection URL to a specific IP address

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-// const socket = io('http://localhost:4000'); // this is the socket server
-const socket = io(process.env.SOCKET_SERVER_URL_STAGING!); // this is the socket server
+const socket = io('http://13.38.78.117:4000'); // this is the socket server
+// const socket = io(process.env.SOCKET_SERVER_URL_STAGING!); // this is the socket server
 
 export default socket;
