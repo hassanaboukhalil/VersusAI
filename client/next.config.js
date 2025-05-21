3bb0631594b3e5592d963b34ad223959e2552a46
hassan <hassanak509@gmail.com>
2025-05-21 08:21:47 +0300

    frontend: added API proxy configuration and allow ESLint/TS errors in CI

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // config options here
+    // allow ESLint/TS errors to pass in CI
     eslint: {
         ignoreDuringBuilds: true,
     },
     typescript: {
         ignoreBuildErrors: true,
+    },
+
+    // this section will proxy /api/* → your HTTP backend
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://13.51.158.192:8000/api/:path*',
+            },
+        ];
     },
 };
 
