2a94db5cd957d057bc928eb277c7ccc4271e84f1
hassan <hassanak509@gmail.com>
2025-08-25 15:43:09 +0300

    frontend: update socket connection to fallback to localhost if SOCKET_SERVER_URL_PRODUCTION is not set

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // const socket = io('/:4000'); // this is the socket server
-const socket = io(process.env.SOCKET_SERVER_URL_PRODUCTION!); // this is the socket server
+const socket = io(process.env.SOCKET_SERVER_URL_PRODUCTION! || 'http://localhost:4000'); // this is the socket server
 
 // const origin = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
 // const socket = io(`http://${origin}:4000`);
