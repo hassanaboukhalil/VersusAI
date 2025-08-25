0aa8baaf3a54eb6b805abab0d50ee6d6ab47fcdd
hassan <hassanak509@gmail.com>
2025-08-25 15:47:48 +0300

    backend: update User model to use Laravel Sanctum for API token management and remove JWT implementation

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -6,12 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -80,15 +80,7 @@ class User extends Authenticatable implements JWTSubject
     }
 
 
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
 
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
     // public function setPasswordAttribute($password)
     // {
     //     if (!empty($password)) {
