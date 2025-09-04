ebb8f23db1ec9e1459d7cf5abf9f6336eb5c3cc6
hassan <hassanak509@gmail.com>
2025-09-04 14:24:16 +0300

    backend: update battles route to accept optional user_id parameter

--- server/routes/api.php
+++ server/routes/api.php
@@ -42,7 +42,7 @@ Route::group(['prefix' => 'v1'], function () {
 
 
             Route::get('/ai-models', [AIModelController::class, 'index']);
-            Route::get('/battles', [BattleController::class, 'getAllBattles']);
+            Route::get('/battles/{user_id?}', [BattleController::class, 'getAllBattles']);
 
             // Route::get('/battles', [BattleController::class, 'index']);
 
