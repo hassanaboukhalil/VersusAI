93de753e987b9a73478b98f4487200823f8db3ca
hassan <hassanak509@gmail.com>
2025-09-04 14:23:05 +0300

    backend: enhance getAllBattles to filter by user_id if provided

--- server/app/Services/BattleService.php
+++ server/app/Services/BattleService.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Auth;
 
 class BattleService
 {
-    public function getAllBattles(): array
+    public function getAllBattles($user_id = null): array
     {
-        $all_battles = Battle::where('is_active', false)->with('user', 'category', 'ai_model_1', 'ai_model_2', 'votes')->orderBy('created_at', 'desc')->get();
+        $all_battles = Battle::where('is_active', false);
+        if ($user_id) {
+            $all_battles = $all_battles->where('user_id', $user_id);
+        }
+        $all_battles = $all_battles->with('user', 'category', 'ai_model_1', 'ai_model_2', 'votes')->orderBy('created_at', 'desc')->get();
 
         $battles = [];
 
@@ -42,6 +46,7 @@ class BattleService
                 'ai_model_2_name' => $battle->ai_model_2->model_name,
                 'votes_ai_model_1' => $votes_ai_model_1,
                 'votes_ai_model_2' => $votes_ai_model_2,
+                'user_id' => $battle->user_id,
                 'user_first_name' => $battle->user->first_name,
                 'user_last_name' => $battle->user->last_name,
                 'user_profile_pic_url' => $battle->user->profile_picture_url,
