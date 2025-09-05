94ca31e33d3d1222e84f1bc3ced72f915f829e66
hassan <hassanak509@gmail.com>
2025-09-05 07:20:01 +0300

    frontend: update ProfilePage to fetch battles by username and render BattleCards

--- client/src/app/(private)/(profile)/[username]/page.tsx
+++ client/src/app/(private)/(profile)/[username]/page.tsx
@@ -8,31 +8,30 @@ import BattleCards from '../../../../components/global/BattleCards';
 import { Battle } from '../../../../types/battle';
 
 const ProfilePage = () => {
-    // const { username } = useParams();
-    // const [battles, setBattles] = useState<Battle[]>([]);
+    const { username } = useParams();
+    const [battles, setBattles] = useState<Battle[]>([]);
 
-    // useEffect(() => {
+    useEffect(() => {
+        // api.get(`/users/${username}`)
+        // .then((res) => {
 
-    //     // api.get(`/users/${username}`)
-    //     // .then((res) => {
+        // })
 
-    //     // })
-
-    //     api.get(`/battles/${user_id}`)
-    //         .then((res) => {
-    //             if (res.data.success) {
-    //                 setBattles(res.data.data);
-    //                 console.log(res.data);
-    //             }
-    //         })
-    //         .catch((error) => console.error('Failed to fetch battles: ', error));
-    // }, []);
+        api.get(`/battles/${username}`)
+            .then((res) => {
+                if (res.data.success) {
+                    setBattles(res.data.data);
+                    console.log(res.data.data);
+                }
+            })
+            .catch((error) => console.error('Failed to fetch battles: ', error));
+    }, []);
 
     return (
         <Section className="bg-background min-h-screen py-12">
             <h1 className="text-white text-3xl font-bold mb-8">Profile Page</h1>
 
-            {/* <BattleCards battles={battles} /> */}
+            <BattleCards battles={battles} />
         </Section>
     );
 };
