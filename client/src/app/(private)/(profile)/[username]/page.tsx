cbf732bff862e251ecdbcb3ee307885107ca2a98
hassan <hassanak509@gmail.com>
2025-09-05 10:48:47 +0300

    frontend: show loading skeleton while fetching battles in ProfilePage

--- client/src/app/(private)/(profile)/[username]/page.tsx
+++ client/src/app/(private)/(profile)/[username]/page.tsx
@@ -5,23 +5,17 @@ import { useEffect, useState } from 'react';
 import api from '../../../../lib/axios';
 import Section from '../../../../components/layout/Section';
 import BattleCards from '../../../../components/global/BattleCards';
-import { Battle } from '../../../../types/battle';
+import { CardsSkeleton } from '../../../../components/ui/Skeletons';
 
 const ProfilePage = () => {
     const { username } = useParams();
-    const [battles, setBattles] = useState<Battle[]>([]);
+    const [battles, setBattles] = useState(null);
 
     useEffect(() => {
-        // api.get(`/users/${username}`)
-        // .then((res) => {
-
-        // })
-
         api.get(`/battles/${username}`)
             .then((res) => {
                 if (res.data.success) {
                     setBattles(res.data.data);
-                    console.log(res.data.data);
                 }
             })
             .catch((error) => console.error('Failed to fetch battles: ', error));
@@ -31,7 +25,7 @@ const ProfilePage = () => {
         <Section className="bg-background min-h-screen py-12">
             <h1 className="text-white text-3xl font-bold mb-8">Profile Page</h1>
 
-            <BattleCards battles={battles} />
+            {!battles ? <CardsSkeleton /> : <BattleCards battles={battles} />}
         </Section>
     );
 };
