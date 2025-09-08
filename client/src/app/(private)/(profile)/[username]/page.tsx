c0ce03e147b159158462aded2c092d199bd485de
hassan <hassanak509@gmail.com>
2025-09-08 15:47:48 +0300

    frontend: enhance ProfilePage with user profile image and edit button

--- client/src/app/(private)/(profile)/[username]/page.tsx
+++ client/src/app/(private)/(profile)/[username]/page.tsx
@@ -7,10 +7,12 @@ import Section from '../../../../components/layout/Section';
 import BattleCards from '../../../../components/global/BattleCards';
 import { CardsSkeleton } from '../../../../components/ui/Skeletons';
 import Image from 'next/image';
+import { Battle } from '../../../../types/battle';
+import { Button } from '../../../../components/ui/button';
 
 const ProfilePage = () => {
     const { username } = useParams();
-    const [battles, setBattles] = useState(null);
+    const [battles, setBattles] = useState<Battle[] | null>(null);
 
     useEffect(() => {
         api.get(`/battles/${username}`)
@@ -26,10 +28,10 @@ const ProfilePage = () => {
     return (
         <Section className="bg-background min-h-screen py-12">
             <h1 className="text-white text-3xl font-bold mb-8">Profile Page</h1>
-            <div>
+            <div className="relative">
                 {Array.isArray(battles) && battles[0]?.user_cover_pic_url && (
                     <Image
-                        className="w-full"
+                        className="w-full h-75"
                         src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_cover_pic_url}`}
                         // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
                         width={626}
@@ -37,9 +39,36 @@ const ProfilePage = () => {
                         alt="cover image"
                     />
                 )}
+
+                {/* {Array.isArray(battles) && battles[0]?.user_profile_pic_url && (
+                    <Image
+                        className="rounded-full mb-4 z-10 w-36 h-36 absolute left-2 top-[75%]"
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_profile_pic_url}`}
+                        // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
+                        width={224}
+                        height={224}
+                        alt="profile image"
+                    />
+                )} */}
+
+                <div className="flex items-end justify-between w-full h-12">
+                    {Array.isArray(battles) && battles[0]?.user_profile_pic_url && (
+                        <Image
+                            className="rounded-full ml-4 z-10 w-36 h-36 border border-black"
+                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_profile_pic_url}`}
+                            // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
+                            width={224}
+                            height={224}
+                            alt="profile image"
+                        />
+                    )}
+                    <Button variant="outline">Edit Profile</Button>
+                </div>
             </div>
 
-            {!battles ? <CardsSkeleton /> : <BattleCards battles={battles} />}
+            <div className="mt-8">
+                {!battles ? <CardsSkeleton /> : <BattleCards battles={battles} />}
+            </div>
         </Section>
     );
 };
