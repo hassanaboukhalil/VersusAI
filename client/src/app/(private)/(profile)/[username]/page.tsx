427c7a969184f03342dc4e86a48561dde1a1d154
hassan <hassanak509@gmail.com>
2025-09-08 16:04:55 +0300

    frontend: refactor ProfilePage layout for improved structure and readability

--- client/src/app/(private)/(profile)/[username]/page.tsx
+++ client/src/app/(private)/(profile)/[username]/page.tsx
@@ -26,48 +26,51 @@ const ProfilePage = () => {
     }, []);
 
     return (
-        <Section className="bg-background min-h-screen py-12">
+        <Section className="bg-background min-h-screen py-12 items-center">
             <h1 className="text-white text-3xl font-bold mb-8">Profile Page</h1>
-            <div className="relative">
-                {Array.isArray(battles) && battles[0]?.user_cover_pic_url && (
-                    <Image
-                        className="w-full h-75"
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_cover_pic_url}`}
-                        // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
-                        width={626}
-                        height={352}
-                        alt="cover image"
-                    />
-                )}
-
-                {/* {Array.isArray(battles) && battles[0]?.user_profile_pic_url && (
-                    <Image
-                        className="rounded-full mb-4 z-10 w-36 h-36 absolute left-2 top-[75%]"
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_profile_pic_url}`}
-                        // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
-                        width={224}
-                        height={224}
-                        alt="profile image"
-                    />
-                )} */}
-
-                <div className="flex items-end justify-between w-full h-12">
-                    {Array.isArray(battles) && battles[0]?.user_profile_pic_url && (
+            {Array.isArray(battles) && battles[0]?.user_cover_pic_url && (
+                <Image
+                    className="w-full h-75"
+                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_cover_pic_url}`}
+                    // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
+                    width={626}
+                    height={352}
+                    alt="cover image"
+                />
+            )}
+            <div className="w-full px-4">
+                {Array.isArray(battles) && battles[0]?.user_profile_pic_url && (
+                    <div className="flex items-end justify-between w-full h-12">
                         <Image
-                            className="rounded-full ml-4 z-10 w-36 h-36 border border-black"
+                            className="rounded-full z-10 w-36 h-36 border border-black"
                             src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${battles[0].user_profile_pic_url}`}
                             // src={`http://localhost:8000/${battles[0].user_cover_pic_url}`}
                             width={224}
                             height={224}
                             alt="profile image"
                         />
-                    )}
-                    <Button variant="outline">Edit Profile</Button>
-                </div>
-            </div>
+                        <Button variant="outline">Edit Profile</Button>
+                    </div>
+                )}
 
-            <div className="mt-8">
-                {!battles ? <CardsSkeleton /> : <BattleCards battles={battles} />}
+                {/* name, username and bio */}
+                <div className="mt-8">
+                    {!battles ? (
+                        ''
+                    ) : (
+                        <>
+                            <h1 className="text-2xl font-bold">
+                                {battles[0]?.user_first_name} {battles[0]?.user_last_name}
+                            </h1>
+                            <p className="text-sm text-gray-500">@{username}</p>
+                            <p className="text-medium pt-2">This is the bio</p>
+                        </>
+                    )}
+                </div>
+
+                <div className="mt-8">
+                    {!battles ? <CardsSkeleton /> : <BattleCards battles={battles} />}
+                </div>
             </div>
         </Section>
     );
