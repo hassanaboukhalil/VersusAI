e40d3466f009533c49e33de2024a07c78588c907
hassan <hassanak509@gmail.com>
2025-10-20 18:20:54 +0300

    frontend: update BattleCards component to display a message when no battles are available

--- client/src/components/global/BattleCards.tsx
+++ client/src/components/global/BattleCards.tsx
@@ -4,11 +4,15 @@ import BattleCard from './BattleCard';
 import { Battle } from '../../types/battle';
 
 const BattleCards = ({ battles }: { battles: Battle[] }) => {
-    return (
+    return battles.length != 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {battles.map((battle) => (
                 <BattleCard key={battle.id} battle={battle} />
             ))}
+        </div>
+    ) : (
+        <div className="w-full flex-center h-96">
+            <h1 className="text-white text-3xl">No Battles Yet</h1>
         </div>
     );
 };
